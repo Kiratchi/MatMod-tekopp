c48e1008863a4f3dc224a7d7619748e472d4f1fb
Jonberto <108332311+Jonberto@users.noreply.github.com>
2023-04-19 16:29:21 +0200

    skapat funktionsfiler för alla färdiga värmeöverföringskonstanter

---
+++ h_cup2air.m
@@ -0,0 +1,17 @@
+function R_hcup2air = h_cup2air(T_out_cup, C_Length) 
+
+    T_air = 293.15; % air is assumed to be constant
+    Pr_air = 0.7309 % vid 20'C källa https://www.engineersedge.com/physics/viscosity_of_air_dynamic_and_kinematic_14483.htm
+    my_air = 1.825*10^-5; % kg / m s vid 20'C
+    rho_air = 1.204 %kg/m^3 vid 20'C
+    k_air = 25.8*10^-3 % W / m K vid 20'C
+    ny_air = my_air / rho_air;
+
+    Ra_L = Pr_air * g * 1/T_air * (T_out_cup-T_air) * C_Length^3 / (ny_air^2)
+
+    Nu_L = 0.68 + 0.67 * (Ra_L^0.25)/(1 + (0.492/Pr_air)^(9/16))^(4/9);
+    
+    R_hcup2air = (k_air / C_Length) * Nu_L
+
+end
+
---
+++ h_l2top.m
@@ -0,0 +1,17 @@
+function R_hl2top = h_l2top(T_top,T_l,C_innerdiam) 
+    T_film = (T_top + T_l)/2
+    g = 9.82; % m / s^2
+    konst_A=1.522; konst_B=243; my_water = konst_A*exp(konst_B/T_film); %undersök enhet i andrade equation
+    rho_water = 1000 % kg/m^3
+    k_water = 0.598; % W / m K vid 20'C
+    ny_water = my_water / rho_water 
+    Pr_water = 50000/(T_film^2 + 155* T_film + 3700);
+    Ra_L = g * Pr_water * 1/T_l * (T_l - T_top) * C_innerdiam/4 / (ny_water^2)
+     if Ra_L >= 10^7 && Ra_L <= 10^11
+        Nu_L = 0.15 * Ra_L^(1/3)
+        R_hliq2surf = (k_water / (C_innerdiam/4))*Nu_L
+      else 
+        error('korrelationen håller ej för då Ra_L är för stor eller liten')
+     end
+end 
+
