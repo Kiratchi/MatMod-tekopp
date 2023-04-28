9a40b54d59e420ddfdd57f3c9c15bbbcf7675ad5
Jonatan Eklöv <jonatan.eklov@gmail.com>
2023-04-28 16:27:45 +0200

    Kommenterat kod bättre

--- Model1.m
+++ Model1.m
@@ -1,11 +1,11 @@
 %% Assumtions
 % Lumped liquid box with air film around
 clc, clear, clf
-
+warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames')
 
 % Physical dimensions
 p.r_inner = 7 *10^(-2); %m
-p.thickness_glass = 0.5; %m
+p.thickness_glass = 0.005; %m
 p.r_outer = p.r_inner + p.thickness_glass; %m
 p.height = 9.5*10^(-2); %m
 p.A_side_l= 2*pi*p.r_inner*p.height; %m^2
@@ -19,7 +19,7 @@ p.volume_l = 250 *10^(-6); %m^3
 p.T_air = 273.15+20.6; %K
 
 % Radiation properies
-p.sftboltz_const = 6.676*10^-8; %W/m^2 K^4 (Transportenboken)
+p.sftboltz_const = 6.676*10^-8; %W/m^2 K^4 (Fundamentals of Momentum, Heat and Mass Transfer)
 p.emissitivity_glass = 0.94;
 p.emissitivity_l = 0.97 ;   %https://journals.ametsoc.org/view/journals/apme/11/8/1520-0450_1972_011_1391_ldowse_2_0_co_2.xml
 p.rad_glass_const= p.A_top_l*p.emissitivity_glass*p.sftboltz_const;
@@ -29,54 +29,39 @@ p.rad_l_const= p.A_top_l*p.emissitivity_l*p.sftboltz_const;
 p.k_glass = 0.9; %J/smK
 
 
-% tspan = [0 2500]; % in s
-% T_t0_l = 273.15+100; %K 
-% [t,y] = ode45(@(t,T) derivate(p,T), tspan, T_t0_l);
-% T = y-273.15;
-% subplot(2,2,[1,2])
-% hold on
-% plot(t, T, 'b','LineWidth',1.5)
-% plot(t, 0*t+p.T_air-273.15, 'b--')
-% axis([tspan 0 100])
-% xlabel("Time (s)")
-% ylabel(" T (C)")
-% title("Solution over time")
-% T(end)
-% 
-% subplot(2,2,3)
-plot_side_temp(p,p.T_air+10, 273.15+100)
-% subplot(2,2,4)
-% plot_top_temp(p,p.T_air, 273.15+100)
+subplot(2,2,[1 2])
+plot_time_solution(p, 273.15+80, [0 2500], true)
+plot_small_data()
+legend('Our solution','Exp 1','Exp 2', 'Exp 3')
+subplot(2,2,3)
+plot_side_temp(p,p.T_air+5, 273.15+100)
+subplot(2,2,4)
+plot_top_temp(p,p.T_air+5, 273.15+100)
 
 
 function dTdt = derivate(p,T_l)
-
-    %T_out_cup = (T_l*R_glass^-1 + p.T_air*R_glass2air^-1) / (R_glass^-1 + R_glass2air^-1); 
-    %T_surf_l = T_l;
     [T_in_cup, T_out_cup] = t_finder_side(p, T_l);
     T_top = t_finder_top(p, T_l);    
 
-
-    C_innerdiam = 2*p.r_inner; % karaktäristisk diameter 
-    h_l2top = calc_h_l2top(T_top,T_l,C_innerdiam); %Fixa utryck
+    C_innerdiam = 2*p.r_inner; % Charateristic diameter 
+    h_l2top = calc_h_l2top(T_top,T_l,C_innerdiam);
     R_l2top = (p.A_side_l*h_l2top)^-1;
     q_l2top = (T_l - T_top)/R_l2top;
 
-    C_L = p.height; % karaktäristisk längd 
-    h_l2cup = calc_h_l2cup(T_in_cup, T_l,C_L); %Fixa utryck
+    C_L = p.height; % Charecteristic length
+    h_l2cup = calc_h_l2cup(T_in_cup, T_l,C_L);
     R_l2glass = (p.A_side_l*h_l2cup)^-1;
     q_l2glass = (T_l - T_in_cup)/R_l2glass;
-
 
     dTdt = -1/(cp_water(T_l)*rho_water(T_l)*p.volume_l)*(q_l2top + q_l2glass);
 end
 
 
 function f = costfunc_top_flow(p,T_l, T_top)
-    C_innerdiam = 2*p.r_inner; % karaktäristisk diameter 
-    h_l2air = calc_h_l2air(T_top,C_innerdiam); %Fixa utryck
+    C_innerdiam = 2*p.r_inner; % Charateristic diameter 
+    h_l2air = calc_h_l2air(T_top,C_innerdiam);
     
-    h_l2top = calc_h_l2top(T_top, T_l, C_innerdiam); %Fixa utryck
+    h_l2top = calc_h_l2top(T_top, T_l, C_innerdiam);
 
     R_l2top = (p.A_side_l*h_l2top)^-1;
     R_top2air = (p.A_top_l*h_l2air)^-1;
@@ -129,13 +114,16 @@ for T_l = T_l_vector
     T_in_cup_vector = [T_in_cup_vector, T_in_cup];
     T_out_cup_vector = [T_out_cup_vector, T_out_cup];
 end
+disp("Avrage diffrence between T_in_cup and T_l is:" + sum(T_l_vector-T_in_cup_vector)/length(T_l_vector))
+disp("Avrage diffrence between T_out_cup and T_l is:" + sum(T_l_vector-T_out_cup_vector)/length(T_l_vector))
+disp("Avrage diffrence between T_out_cup and T_in_cup is:" + sum(T_in_cup_vector-T_out_cup_vector)/length(T_in_cup_vector))
 plot(T_l_vector-273.15, T_in_cup_vector-273.15,'-b','linewidth', 1.5)
 hold on 
 plot(T_l_vector-273.15, T_out_cup_vector-273.15,'-r','LineWidth',1.5)
 xlabel("T_l (C)")
 ylabel("T_{cup} (C)")
 legend("Outside", "Inside")
-title("Corelation T_l & Side temp")
+title("Correlation T_l & Side temp")
 end
 
 
@@ -146,11 +134,27 @@ for T_l = T_l_vector
     T_top = t_finder_top(p, T_l);
     T_top_vector = [T_top_vector,T_top ];
 end
+disp("Avrage diffrence between T_top and T_l is: " + sum(T_l_vector-T_top_vector)/length(T_l_vector))
 plot(T_l_vector-273.15, T_top_vector-273.15,'-b','linewidth', 1.5)
 hold on 
 xlabel("T_l (C)")
 ylabel("T_{top} (C)")
-title("Corelation T_l & top temp")
+title("Correlation T_l & top temp")
 end
 
+function plot_time_solution(p, T_t0_l, t_span, Includelabels) 
+    [t,y] = ode45(@(t,T) derivate(p,T), t_span, T_t0_l);
+    T = y-273.15;
+    hold on
+    plot(t, T,'LineWidth',1.5)
+    %plot(t, 0*t+p.T_air-273.15, '--', 'Color', '#808080')
+    try
+    if Includelabels ~=  false
+        axis([t_span 0 100])
+        xlabel("Time (s)")
+        ylabel(" T (C)")
+        title("Solution over time")
+    end
+    end
+end
 
