340e4921596799488282133fbe78717314812778
Jonberto <108332311+Jonberto@users.noreply.github.com>
2023-05-03 09:50:17 +0200

    ändra radie till diameter

--- calc_kc.m
+++ calc_kc.m
@@ -4,5 +4,5 @@ T_film = (T_top + p.T_air)/2;
 D_AB = 2.634 * 1 / 101325 * (T_film/293)^(3/2);
 ny_water=my_water(T_film)/rho_water(T_film);
 Sc = ny_water / D_AB;
-R_calc_kc = calc_h_l2air(T_top,p.r_inner)/(rho_air(T_film) * cp_air(T_film))*(pr_water(T_film)/Sc)^(2/3);
+R_calc_kc = calc_h_l2air(T_top,p.r_inner*2)/(rho_air(T_film) * cp_air(T_film))*(pr_water(T_film)/Sc)^(2/3);
 end
