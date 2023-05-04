79e0bdd8fb361ff8ba8a1b7a9c64617f0946e723
Jonberto <108332311+Jonberto@users.noreply.github.com>
2023-05-04 10:44:09 +0200

    funktion för evap och stefanflöde

---
+++ calc_n_A.m
@@ -0,0 +1,8 @@
+function n_A = calc_n_A(T_top,p)
+C_air = p.RH * p_water(T_air)/(p.R*T_air);
+C_surf = p_water(T_top)/(p.R*T_top);
+n_A = calc_kc(Top,p)*p.A_top_l * p.M_water * (C_surf - C_air);
+C_tot_surf = p.p_tot / (p.R*T_top);
+mol_frac = C_surf / C_tot_surf;
+n_A = n_A/(1-mol_frac);
+end
