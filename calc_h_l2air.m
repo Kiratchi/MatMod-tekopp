252317fbad0b21e6e27fb8cc4aa279ba735fe482
Jonberto <108332311+Jonberto@users.noreply.github.com>
2023-05-17 09:02:47 +0200

    ändra T_top

--- calc_h_l2air.m
+++ calc_h_l2air.m
@@ -7,7 +7,7 @@ function R_h_l2air = calc_h_l2air(T_top,C_innerdiam)
 
 
     Pr_air = (my_air(T_air).*cp_air(T_air))./k_air(T_air);
-    Ra_L = g * Pr_air *(T_top - T_air) *1/T_air * (C_innerdiam/4)^3 / ((ny_air^2));
+    Ra_L = g * Pr_air *(T_film - T_air) *1/T_air * (C_innerdiam/4)^3 / ((ny_air^2));
 
     if (Ra_L >= 10^7) && (Ra_L <= 10^11)
         Nu_L = 0.15 * Ra_L^(1/3);
@@ -16,7 +16,7 @@ function R_h_l2air = calc_h_l2air(T_top,C_innerdiam)
         Nu_L = 0.54 * Ra_L^(1/4);
         R_h_l2air = (k_air(T_film) / (C_innerdiam/4)) * Nu_L;
     elseif (290 <= Ra_L) && (Ra_L <= 3.3*10^5)
-        Nu_L = 0.78+0.82*Ra^(1/5); % källa: Martorell et al (2003) 
+        Nu_L = 0.78+0.82*Ra_L^(1/5); % källa: Martorell et al (2003) 
         R_h_l2air = (k_air(T_film) / (C_innerdiam/4)) * Nu_L;
     else
         %warning('the correlation does not  hold for the interval')
